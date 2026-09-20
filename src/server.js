d30e26702224bf8648776a4264efa9e3288e9cc4
Anastasiia Aharkova <agarkova.anastasiya2007@gmail.com>
2026-09-20 14:50:50 +0200

    Change commit directory

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ import cookieParser from "cookie-parser";
 
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 
 
